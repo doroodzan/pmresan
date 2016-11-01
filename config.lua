b2b275fbc398c0a07f0e062f20ea2d04cd628270
Doroodzan <info@doroodzan.ir>
2016-11-01 17:59:10 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
-bot_token = "245451534:AAEgrx-zGY_wRWkR0qYHi2xjSNL-qEjMZFU"
+bot_token = "270104566:AAFBjY4nkVIc-_KvgVqpQdCcZ0Jjqft0sVk"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "6.0"
-sudo_name = "Engineer Shayan Ahmadi"
-sudo_id = 259300954
-admingp = -135847900
-sudo_num = "989351372038"
-sudo_user = "shayan_soft"
-sudo_ch = "UmbrellaTeam"
+sudo_name = "Engineer Matin"
+sudo_id = 231944587
+admingp = -154017155
+sudo_num = "+79685998194"
+sudo_user = "MrDoctoriQ"
+sudo_ch = "Webmagonline"
