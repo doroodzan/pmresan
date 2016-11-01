3a843f40b29ba9e5f8c23a77b8fdd1d32643302d
Doroodzan <info@doroodzan.ir>
2016-11-01 17:40:45 +0330

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,9 @@
+bot_token = "245451534:AAEgrx-zGY_wRWkR0qYHi2xjSNL-qEjMZFU"
+send_api = "https://api.telegram.org/bot"..bot_token
+bot_version = "6.0"
+sudo_name = "Engineer Shayan Ahmadi"
+sudo_id = 259300954
+admingp = -135847900
+sudo_num = "989351372038"
+sudo_user = "shayan_soft"
+sudo_ch = "UmbrellaTeam"
